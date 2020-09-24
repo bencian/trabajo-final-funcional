53e02a4a06bb531d239ade4f1d39006a7655a54e
bencian <bencian94@gmail.com>
2020-09-24 19:25:37 -0300

    started adding our files

--- app/podrida.hs
+++ app/podrida.hs
@@ -1,13 +1,31 @@
 import qualified Graphics.UI.Threepenny as UI
 import Graphics.UI.Threepenny.Core
 
+import Control.Monad
+
+import System.FilePath
+import System.IO
 
 -- | Main entry point.
 main :: IO ()
-main = startGUI defaultConfig setup
+main = do
+    static <- getStaticDir
+    startGUI defaultConfig { jsStatic = Just static } setup
 
 setup :: Window -> UI ()
-setup w = do
+setup w = void $ do
     -- active elements
     return w # set title "Podrida"
-    return ()
+    UI.addStyleSheet w "podrida.css"
+    
+    getBody w #+
+        [UI.div #. "wrap" #+ greet]
+
+greet :: [UI Element]
+greet =
+    [ UI.h1  #+ [string "Bienvenido al anotador piola!"]
+    , UI.div #+ [string "Para jugar, ingrese 1."]
+    ]
+
+getStaticDir :: IO FilePath
+getStaticDir = return "static"
