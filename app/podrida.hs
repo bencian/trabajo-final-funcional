ad1c61a9b190def3263b20419e374bb4d089f08b
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2020-09-24 18:21:21 -0300

    Agrego carpeta app para los archivos de nuestra app, y creo archivo inicial podrida.sh

---
+++ app/podrida.hs
@@ -0,0 +1,13 @@
+import qualified Graphics.UI.Threepenny as UI
+import Graphics.UI.Threepenny.Core
+
+
+-- | Main entry point.
+main :: IO ()
+main = startGUI defaultConfig setup
+
+setup :: Window -> UI ()
+setup w = do
+    -- active elements
+    return w # set title "Podrida"
+    return ()
