7c1fb5b93f37e25525994580934f927e9e4b8168
bencian <bencian94@gmail.com>
2020-09-29 19:59:58 -0300

    style and frontend changes

--- app/podrida.hs
+++ app/podrida.hs
@@ -17,14 +17,27 @@ setup w = void $ do
     -- active elements
     return w # set title "Podrida"
     UI.addStyleSheet w "podrida.css"
-    
-    getBody w #+
-        [UI.div #. "wrap" #+ greet]
+    UI.addStyleSheet w "bootstrap.css"
+
+    getBody w #+ greet #+ button_container
 
 greet :: [UI Element]
 greet =
-    [ UI.h1  #+ [string "Bienvenido al anotador piola!"]
-    , UI.div #+ [string "Para jugar, ingrese 1."]
+    [ UI.h1 #+ [string "Bienvenido al anotador piola!" ] # set UI.class_ "text-center text-light" ]
+
+button_container :: [UI Element]
+button_container =
+    [ UI.div # set UI.class_ "container-fluid" #+ button_row ]
+
+button_row :: [UI Element]
+button_row =
+    [ UI.div # set UI.class_ "row text-center" #+ buttons ]
+
+buttons :: [UI Element]
+buttons = 
+    [ UI.a #+ [string "Cargar juego"] # set UI.class_ "btn_init_page btn col-4" # set UI.id_ "load_game"
+    , UI.a #+ [string "Nuevo juego"] # set UI.class_ "btn_init_page btn col-4" # set UI.id_ "new_game"
+    , UI.a #+ [string "Estadísticas"] # set UI.class_ "btn_init_page btn col-4" # set UI.id_ "statistics"
     ]
 
 getStaticDir :: IO FilePath
