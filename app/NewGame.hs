47687edbd7eb94cb82228a79831d324b13d56e97
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2020-10-29 21:05:32 -0300

    Fix passing game name from new_game screen to play_game screen. 	Add new styles 	Show players list in play game screen

--- app/NewGame.hs
+++ app/NewGame.hs
@@ -14,15 +14,17 @@ module NewGame (render_new_game) where
   render_new_game :: (Window -> UI ()) -> Window -> UI ()
   render_new_game setup w = void $ do
     return w # set title "Nuevo Juego"
-    UI.addStyleSheet w "podrida.css"
     UI.addStyleSheet w "bootstrap.css"
+    UI.addStyleSheet w "new_game.css"
     
     inputs <- liftIO $ newIORef []
-    add_game_button_element <- add_player_button
-    remove_game_button_element <- remove_player_button
-    getBody w #+ main_div (button_container (main_menu_button ++ play_game_button) ++ game_name_div ++ (new_game_form (map element [add_game_button_element, remove_game_button_element])))
+
+    add_player_button <- add_player_btn
+    remove_player_button <- remove_player_btn
+
+    getBody w #+ main_div (game_name_form ++ add_game_players_form add_player_button remove_player_button ++ add_button_container)
     
-    game_name_element <- getElementById w "title"
+    game_name_element <- getElementById w "game_title"
     form_div_element <- getElementById w "form_div"
 
     let
@@ -50,21 +52,31 @@ module NewGame (render_new_game) where
     redirect_to_button "main_menu" setup w
     redirect_to_button "play_game" (render_play_game game_title player_list setup) w 
 
-    on UI.click add_game_button_element $ \_ -> addInput >> redo_layout
-    on UI.click remove_game_button_element $ \_ -> removeInput >> redo_layout
+    on UI.click add_player_button $ \_ -> addInput >> redo_layout
+    on UI.click remove_player_button $ \_ -> removeInput >> redo_layout
 
-  new_game_form :: [UI Element] -> [UI Element]
-  new_game_form xs = 
-      [ UI.h4 # set UI.text "Nombres de los jugadores", UI.div # set UI.id_ "form_div" ] ++ xs
+  game_name_form :: [UI Element]
+  game_name_form = [UI.h3 # set UI.text "Nombre del juego " # set UI.id_ "game_name", UI.input # set UI.id_ "game_title"]
 
-  game_name_div :: [UI Element]
-  game_name_div = [UI.h4 # set UI.text "Nombre del juego: ", UI.input # set UI.id_ "title"]
+  game_players_form :: [Element] -> [UI Element]
+  game_players_form buttons =
+      [ UI.h3 # set UI.text "Nombres de los jugadores" # set UI.id_ "players_title", add_remove_btns_div buttons, UI.div # set UI.id_ "form_div" ]
+
+  add_remove_btns_div :: [Element] -> UI Element
+  add_remove_btns_div buttons = UI.div # set UI.id_ "add_and_remove_buttons" #+ map element buttons
   
-  add_player_button :: UI Element
-  add_player_button = UI.button # set UI.text "Agregar" # set UI.class_ "btn_init_page btn" # set UI.id_ "add_player"
+  add_player_btn :: UI Element
+  add_player_btn = UI.button # set UI.text "Agregar" # set UI.class_ "btn btn-success" # set UI.id_ "add_player"
 
-  remove_player_button :: UI Element
-  remove_player_button = UI.button # set UI.text "Eliminar" # set UI.class_ "btn_init_page btn" # set UI.id_ ("remove_player")
+  remove_player_btn :: UI Element
+  remove_player_btn = UI.button # set UI.text "Eliminar" # set UI.class_ "btn btn-danger" # set UI.id_ ("remove_player")
+
+  add_button_container :: [UI Element]
+  add_button_container = button_container (main_menu_button ++ play_game_button)
+
+  add_game_players_form :: Element -> Element -> [UI Element]
+  add_game_players_form add_player_button remove_player_button = game_players_form [add_player_button, remove_player_button]
+
 
   -- start_game :: Game
   -- start_game w = do
--- app/PlayGame.hs
+++ app/PlayGame.hs
@@ -10,15 +10,25 @@ module PlayGame (render_play_game) where
   import Shared
 
   render_play_game :: UI String -> UI [String] -> (Window -> UI ()) -> Window -> UI ()
-  render_play_game game player_list setup w = void $ do
+  render_play_game game_title player_list setup w = void $ do
     return w # set title "Jugando"
-    UI.addStyleSheet w "podrida.css"
     UI.addStyleSheet w "bootstrap.css"
-    
-    -- getBody w #+ main_div ((button_container main_menu_button) ++ [greet game])   
+    UI.addStyleSheet w "play_game.css"
+
+    title <- game_title
+
+    getBody w #+ main_div ([greet title] ++ players_container player_list ++ button_container main_menu_button)
     
     redirect_to_button "main_menu" setup w
   
   greet :: String -> UI Element
   greet name =
-    UI.h1 #+ [string name ] # set UI.class_ "text-center text-light"
+    UI.h2 #+ [string name ] # set UI.class_ "text-center text-light"
+
+  players_container :: UI [String] -> [UI Element]
+  players_container player_list = [UI.h4 # set UI.text "Jugadores: ", players_ul player_list]
+
+  players_ul :: UI [String] -> UI Element
+  players_ul players_list = do
+    players <- players_list
+    UI.ul # set UI.id_ "players_ul" #+ map (\name -> UI.li #+ [string name]) players
