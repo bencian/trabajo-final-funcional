9b42cc4cd284a91f347bfaa6d08d046e7f5b46be
bencian <bencian94@gmail.com>
2020-11-17 19:52:55 -0300

    adds main game behaviour and some prediction validations

--- app/PlayGame.hs
+++ app/PlayGame.hs
@@ -6,6 +6,7 @@ module PlayGame (render_play_game) where
 
   import System.FilePath
   import System.IO
+  import Data.IORef
 
   import Shared
   import SharedBackend
@@ -16,9 +17,10 @@ module PlayGame (render_play_game) where
     UI.addStyleSheet w "bootstrap.css"
     UI.addStyleSheet w "play_game.css"
 
-    -- inputs <- liftIO $ newIORef []
     title <- game_title
     game <- ui_game
+    mutable_game <- liftIO $ newIORef game
+    
     getBody w #+ main_div ([greet title, board_div] ++ action_buttons ++ button_container main_menu_button)
     
     confirm_predicted_rounds <- getElementById w "confirm_predicted_rounds"
@@ -29,28 +31,42 @@ module PlayGame (render_play_game) where
       input_list :: UI [String]
       input_list = mapM (get value) =<< getElementsByTagName w "input"
 
-      redo_layout :: Game -> UI ()
-      redo_layout game = void $ do
-        board_element <- board #+ (table_elements game)
+      redo_layout :: IORef Game -> String -> UI ()
+      redo_layout ioref_game string = void $ do
+        game <- liftIO (readIORef ioref_game)
+        board_element <- board #+ (table_elements game string)
         element (from_just board_div_element) # set children [board_element]
+
+      correct_inputs :: Game -> [String] -> UI ()
+      correct_inputs game_value values_list = do
+        (element (from_just confirm_predicted_rounds)) #. "hide btn btn-sm"
+        (element (from_just confirm_won_rounds)) #. "btn btn-sm"
+        liftIO $ writeIORef mutable_game (insert_predicted game_value values_list)
+        redo_layout mutable_game ("Ingresar ganadas para " ++ show (number_of_cards_round (fst game_value)))
+
+      incorrect_inputs :: Game -> UI ()
+      incorrect_inputs game_value = do
+        redo_layout mutable_game ("Ingresar predicciones para " ++ show (number_of_cards_round (fst game_value)))
         
 
-    redo_layout game
+    redo_layout mutable_game "Ingresar predicciones para 1"
 
     redirect_to_button "main_menu" setup w
   
     on UI.click (from_just confirm_predicted_rounds) $ const $ do
       values_list <- input_list
-      -- if else validation
-      (element (from_just confirm_predicted_rounds)) #. "hide btn btn-sm"
-      (element (from_just confirm_won_rounds)) #. "btn btn-sm"
-      redo_layout (insert_predicted game values_list)
+      game_value <- liftIO $ readIORef mutable_game
+      if' (valid_values values_list (number_of_cards_round (fst game_value))) (correct_inputs game_value values_list) (incorrect_inputs game_value)      
+      
 
-    -- on UI.click (from_just confirm_won_rounds) $ const $ do
-    --   values_list <- input_list
-    --   (element (from_just confirm_predicted_rounds)) #. "btn btn-sm"
-    --   (element (from_just confirm_won_rounds)) #. "hide btn btn-sm"
-    --   redo_layout (insert_winnings (game values_list))
+    on UI.click (from_just confirm_won_rounds) $ const $ do
+      values_list <- input_list
+      game_value <- liftIO $ readIORef mutable_game
+      -- if else validation
+      (element (from_just confirm_predicted_rounds)) #. "btn btn-sm"
+      (element (from_just confirm_won_rounds)) #. "hide btn btn-sm"
+      liftIO $ writeIORef mutable_game (insert_winnings game_value values_list)
+      redo_layout mutable_game ("Ingresar predicciones para " ++ show (number_of_cards_round (fst game_value)))
 
   greet :: String -> UI Element
   greet name =
@@ -60,10 +76,10 @@ module PlayGame (render_play_game) where
   action_buttons = [confirm_predicted_button, confirm_won_button]
 
   confirm_predicted_button =
-    UI.button # set UI.text "Predicciones" #. "btn btn-sm" # set UI.id_ "confirm_predicted_rounds"
+    UI.button # set UI.text "Confirmar predicciones" #. "btn btn-sm" # set UI.id_ "confirm_predicted_rounds"
 
   confirm_won_button =
-    UI.button # set UI.text "Ganadas" #. "hide btn btn-sm" # set UI.id_ "confirm_won_rounds"
+    UI.button # set UI.text "Confirmar ganadas" #. "hide btn btn-sm" # set UI.id_ "confirm_won_rounds"
 
   board_div :: UI Element
   board_div =  UI.div # set UI.id_ "board_div"
@@ -79,15 +95,24 @@ module PlayGame (render_play_game) where
   players_td_list :: [String] -> [UI Element]
   players_td_list players = map (\name -> UI.td #+ [string name]) players
 
-  create_inputs :: Game -> [UI Element]
-  create_inputs game = 
-    [UI.tr # set UI.id_ "game_inputs" #+ ((UI.td # set UI.id_ "board_headers" # set UI.text (show (number_of_cards_round (fst game)))):(create_inputs_row (snd game)))]
+  create_inputs :: Game -> String -> [UI Element]
+  create_inputs game string = 
+    [UI.tr # set UI.id_ "game_inputs" #+ ((UI.td # set UI.id_ "input_header" # set UI.text string):(create_inputs_row (snd game)))]
     
   create_inputs_row :: [String] -> [UI Element]
   create_inputs_row = map (\(x:xs) -> UI.td #+ [UI.input # set UI.id_ (show x)])
 
-  table_elements :: Game -> [UI Element]
-  table_elements game = (board_header game) ++ (create_table_rows (reverse (fst game))) ++ (create_inputs game)
+  table_elements :: Game -> String -> [UI Element]
+  table_elements game string = (board_header game) ++ (create_table_rows (reverse (fst game))) ++ (create_scores game) ++ (create_inputs game string)
+
+  create_scores :: Game -> [UI Element]
+  create_scores game = [ UI.tr # set UI.id_ "game_scores" #+ ((UI.td # set UI.id_ "score_headers" # set UI.text "Puntaje: "):(create_scores_row game)) ]
+
+  create_scores_row :: Game -> [UI Element]
+  create_scores_row game = map (partial_score_td (fst game)) (snd game)
+
+  partial_score_td :: [Round] -> String -> UI Element
+  partial_score_td game name = UI.td #+ [UI.p # set UI.text (show (get_player_score game name))]
 
   create_table_rows :: [Round] -> [UI Element]
   create_table_rows [] = []
@@ -97,8 +122,9 @@ module PlayGame (render_play_game) where
   create_columns [] = []
   create_columns ((x,y,z):round) = (UI.td #+ [ UI.p # set UI.text (show y), UI.p # set UI.text (show z)]):(create_columns round)
 
-  -- insert_winnings :: Game -> [String] -> Game
+  insert_winnings :: Game -> [String] -> Game
+  insert_winnings (rounds, players) values = ((reverse (((insert_values_into_round (head (reverse rounds)) values):(tail (reverse rounds))))), players)
   
   insert_predicted :: Game -> [String] -> Game
-  insert_predicted (rounds, players) values = ((create_round players values):rounds, players)
+  insert_predicted (rounds, players) values = (rounds ++ [create_round players values], players)
 
--- app/SharedBackend.hs
+++ app/SharedBackend.hs
@@ -47,3 +47,28 @@ module SharedBackend where
   insert_values_into_round :: Round -> [String] -> Round
   insert_values_into_round [] [] = []
   insert_values_into_round ((name,predicted,won):player_tuples) (value:values) = (name,predicted,(read value :: Int)):(insert_values_into_round player_tuples values) 
+
+  get_player_score :: [Round] -> String -> Int
+  get_player_score game name = foldr (\round -> (+) (get_round_player_score round name)) 0 game
+
+  get_round_player_score :: Round -> String -> Int
+  get_round_player_score round name = get_won (get_player_tuple round name) + (bonification_points (get_player_tuple round name))
+
+  get_player_tuple :: Round -> String -> Player
+  get_player_tuple round name = head (filter (\(x,y,z) -> x == name) round)
+
+  bonification_points :: Player -> Int
+  bonification_points tuple = if get_predicted tuple == get_won tuple then (if (get_predicted tuple) > 3 then 20 else 10) else 0 
+
+  get_predicted :: Player -> Int
+  get_predicted = \(x,y,z) -> y
+
+  get_won :: Player -> Int
+  get_won = \(x,y,z) -> z
+
+  valid_values :: [String] -> Int -> Bool
+  valid_values values_list number_of_cards = sum (map (\x -> read x :: Int) values_list) /= number_of_cards
+
+  if' :: Bool -> a -> a -> a
+  if' True  x _ = x
+  if' False _ y = y
