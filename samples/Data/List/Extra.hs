ae0e673d8f6ebf7eb0f27f667aed680ca903c2e1
Benjamin Ciancio <bencian94@gmail.com>
2020-12-10 19:57:59 -0300

    removed threepenny samples folder

--- samples/Data/List/Extra.hs
+++
@@ -1,8 +0,0 @@
-module Data.List.Extra where
-                  
-import Data.List
-import Data.Char
-import Data.Maybe
-
-trim :: String -> String
-trim = reverse . dropWhile isSpace . reverse . dropWhile isSpace
